412e72f5d896f86b87b376316c7c1c542e0e6355
Arsher123 <58815421+Arsher123@users.noreply.github.com>
2020-04-07 20:07:38 +0200

    Delete zadanie 10.py

--- Zadania cw3/zadanie 10.py
+++
@@ -1,9 +0,0 @@
-def lista(**ilosc):
-    suma=0;
-    for cos in ilosc:
-        if isinstance (ilosc[cos],list):
-            suma += len(ilosc[cos])
-        else:
-            suma+=1
-    return suma
-print(lista(slodycze="cukierki", pieczywo=["chleb","bulki"]))
