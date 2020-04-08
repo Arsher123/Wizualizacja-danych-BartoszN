8235659083c2748b538926a42edde1039e44fb27
Arsher123 <58815421+Arsher123@users.noreply.github.com>
2020-04-08 18:51:54 +0200

    Add files via upload

---
+++ Zadania cw3/zadanie 11.py
@@ -0,0 +1,18 @@
+import math
+from liczbyzespolone.modul11zwroc import *
+from liczbyzespolone.modul11dod import *
+a=input("podaj liczbe zespolona rzeczywista: ")
+b=input("podaj liczbe zespolona czesc urojona: ")
+print(funkcja1(a,b))
+print(funkcja2(a,b))
+
+liczba1=input("podaj czesc rzeczywista pierwszej liczby: ")
+liczba1=int(liczba1)
+liczba1a=input("podaj czesc urojona pierwszej liczby: ")
+liczba1a=int(liczba1a)
+liczba2=input("podaj  czesc rzeczywista drugiej liczby: ")
+liczba2=int(liczba2)
+liczba2a=input("podaj czesc urojona drugiej liczby: ")
+liczba2a=int(liczba2a)
+znak=input("podaj znak: ")
+print(funkcja3(liczba1,liczba1a,liczba2,liczba2a,znak))
